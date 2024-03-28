01d013c70fe4e2c2e05690ce622d9fdd4b5e9dc4
lch_front <chdeen@naver.com>
2024-03-28 09:53:35 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
-import Home from "./pages/Home";
+//import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import Explore from "./pages/Explore";
+import Joinmember from "./pages/Joinmember";
+import Login from "./pages/Login";
+import LoginTest from "./pages/LoginTest";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          {/* <Route path="/" element={<Login />} /> */}
+          <Route path="/" element={<Login />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/joinmember" element={<Joinmember />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/loginTest" element={<LoginTest />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
